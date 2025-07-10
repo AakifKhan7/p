7a96bc16e12b1eaf08040e0bf9ffa41b45b1a8b2
Aakifkhan7 <baloochaakifkhan@gmail.com>
2025-07-10 23:31:03 +0530

    created spring boot project

---
+++ server/src/main/java/com/Aakifkhan/Rentally/RentallyApplication.java
@@ -0,0 +1,13 @@
+package com.Aakifkhan.Rentally;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RentallyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RentallyApplication.class, args);
+	}
+
+}
---
+++ server/src/test/java/com/Aakifkhan/Rentally/RentallyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.Aakifkhan.Rentally;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RentallyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
